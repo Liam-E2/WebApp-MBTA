9526d272b7a25ecd18c8a8b196aeea3d9ced49dc
Liam Earley <50093863+Liam-E2@users.noreply.github.com>
2020-11-27 16:47:29 -0500

    updated
    updated

--- app.py
+++ app.py
@@ -1,13 +1,55 @@
+from mbta_helper import find_stop_near
+
 """
 Simple "Hello, World" application using Flask
 """
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from werkzeug.exceptions import InternalServerError
+
 
 
 app = Flask(__name__)
-
+    
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('main.html')
+
+
+@app.route('/post/<place_name>')
+def show_post(place_name):
+    # show the post with the given id, the id is an integer
+    pn = find_stop_near(place_name)
+    
+    return render_template('response.html', place_name=pn)
+
+
+@app.route("/find_stop", methods=['POST'])
+def find_stop():
+    """Triggered by HTML form, returns template for response"""
+    # Extract form data
+    loc = request.form['location']
+    station_type = request.form['type']
+
+    # Default no station type
+    if station_type[0] == "S":
+        station_type = None
+    
+    # Find Stop & Populate Template
+    result = find_stop_near(loc, station_type=station_type)
+    
+    return render_template("response.html", place_name=result[0], wheelchair_accessible=result[1])
+
+
+@app.errorhandler(InternalServerError)
+def handle_500(e):
+    """From flask docs for generic 500 error handler; redirects internal server errors to a default error page"""
+    original = getattr(e, "original_exception", None)
+
+    if original is None:
+        # direct 500 error, such as abort(500)
+        return render_template("error.html"), 500
+
+    # wrapped unhandled error
+    return render_template("error.html"), 500
--- mbta_helper.py
+++ mbta_helper.py
@@ -1,10 +1,15 @@
+import json 
+import requests
+from pprint import pprint
+import numpy as np
+
 # Useful URLs (you need to add the appropriate parameters for your requests)
 MAPQUEST_BASE_URL = "http://www.mapquestapi.com/geocoding/v1/address"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 
 # Your API KEYS (you need to use your own keys - very long random characters)
-MAPQUEST_API_KEY = ""
-MBTA_API_KEY = ""
+MAPQUEST_API_KEY = "auSa1xsiSJl3l1TCt5Ci17FalaRHqXGF"
+MBTA_API_KEY = "fd5181087ae743e284ccc86652d81e21"
 
 
 # A little bit of scaffolding if you want to use it
@@ -12,9 +17,11 @@ MBTA_API_KEY = ""
 def get_json(url):
     """
     Given a properly formatted URL for a JSON web API request, return
-    a Python JSON object containing the response to that request.
+    a Python dict object containing the response to that request.
     """
-    pass
+    response = requests.get(url)
+
+    return response.json()
 
 
 def get_lat_long(place_name):
@@ -24,31 +31,85 @@ def get_lat_long(place_name):
     See https://developer.mapquest.com/documentation/geocoding-api/address/get/
     for Mapquest Geocoding  API URL formatting requirements.
     """
-    pass
+    js = get_json(f"{MAPQUEST_BASE_URL}?key={MAPQUEST_API_KEY}&location={place_name}")
+
+    return tuple([v for v in js['results'][0]['locations'][0]['latLng'].values()])
 
 
-def get_nearest_station(latitude, longitude):
+def get_nearest_station(latitude, longitude, station_type=None):
     """
     Given latitude and longitude strings, return a (station_name, wheelchair_accessible)
     tuple for the nearest MBTA station to the given coordinates.
     See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL
-    formatting requirements for the 'GET /stops' API.
+    formatting requirements for the 'GET /stops' API. 
+
+    Optional: Provide station_type to filter by station type. Valid options are:
+
+    0 - Tram, Streetcar, Light rail. Any light rail or street level system within a metropolitan area.
+    1 - Subway, Metro. Any underground rail system within a metropolitan area.
+    2 - Rail. Used for intercity or long-distance travel.
+    3 - Bus. Used for short- and long-distance bus routes.
+    4 - Ferry. Used for short- and long-distance boat service.
     """
-    pass
+
+    # Convert str position to floats
+    pos = [float(i) for i in (latitude, longitude)]
+
+    # Construct//Execute API call
+    url = f"{MBTA_BASE_URL}?latitude={pos[0]}?longitude={pos[1]}?radius=0.01?route_type={station_type}?api_key={MBTA_API_KEY}?limit=300?format=json"
+
+    stations = get_json(url)['data']
+
+    if station_type != None:
+        stations = [s for s in stations if str(s['attributes']['vehicle_type']) == str(station_type)]
+
+    # Functions to extract lat/long tuples and calculate distance to home base
+    def process_station(s):
+        """Given s, a station in the JSON provided by get_json, computes the euclidean distance 
+        between the station and your provided location"""
+
+        # Euclidean distance/l2 norm without the sqrt for efficiency since it's monotonic
+        l2 = lambda x: sum([(s - p)**2 for s, p in zip(x, pos)])
+
+        try:
+            ll = (float(s['attributes']['latitude']), float(s['attributes']['longitude']))
+            return l2(ll)
+
+        except Exception as e:
+            # If mbta API doesn't have station location, replace with large distance
+
+            return 9999999
 
 
-def find_stop_near(place_name):
+    station_distances = np.asarray([process_station(s) for s in stations])
+    closest = stations[np.argmin(station_distances)]
+
+    return [closest['attributes']['name'], closest['attributes']['wheelchair_boarding']]
+
+
+def find_stop_near(place_name, station_type=None):
     """
     Given a place name or address, return the nearest MBTA stop and whether it is wheelchair accessible.
+    0	No Information
+    1	Accessible (if trip is wheelchair accessible)
+    2	Inaccessible
     """
-    pass
+    wheelchair_map = {0: "did not provide accessibility information",
+                      1: "is Wheelchair Accessible",
+                      2: "is Wheelchair Inaccessible"}
+
+    la, lo = get_lat_long(place_name)
+    ns = get_nearest_station(la, lo, station_type)
+    ns[1] = wheelchair_map[ns[1]]
+
+    return ns
 
 
 def main():
     """
     You can test all the functions here
     """
-    pass
+    print(find_stop_near("Fenway Baseball Park Boston"))
 
 
 if __name__ == '__main__':
